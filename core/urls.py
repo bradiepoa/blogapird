f484d9d25062e4f90ac36af9eb52ecc0f5f8b578
bradiepoa <bradiepoa7@gmail.com>
2023-07-23 01:49:21 +0300

    added resolved the issue

--- core/urls.py
+++ core/urls.py
@@ -2,7 +2,8 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', include('blog.urls', namespace='blog')),
-    path('api/', include('blog_api.urls', namespace='blog_api'))
+    path('api/', include('blog_api.urls', namespace='blog_api')),
+    path('admin/', admin.site.urls)
+    
 ]
